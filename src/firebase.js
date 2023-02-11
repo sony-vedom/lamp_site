e7fb8dfe71d6dc207359be851797610cb4a7cd7c
sony-vedom <minerva.lankmiller@gmail.com>
2023-02-11 16:16:49 +0300

    fix bud

--- src/firebase.js
+++ src/firebase.js
@@ -30,16 +30,16 @@ export const onSubmitLogin = (e) => {
         // const token = credential.accessToken;
         //
         // // The signed-in user info.
-        // const user = result.user;
-        // console.log(user)
+        const user = result.user;
+        console.log(user)
         // // IdP data available using getAdditionalUserInfo(result)
         // // ...
 
         console.log(result)
     }).catch((error) => {
         // // Handle Errors here.
-        // const errorCode = error.code;
-        // console.log(errorCode)
+        const errorCode = error.code;
+        console.log(errorCode)
         // const errorMessage = error.message;
         // // The email of the user's account used.
         // const email = error.customData.email;
